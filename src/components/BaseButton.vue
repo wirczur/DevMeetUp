b9f33b028eb5c5d9e72fb74f890861ad68fccfe5
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 11:44:58 +0100

    [#2.1] Dodanie przycisku

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="base-button">
+    <el-button
+      :loading="loading"
+      @click="clickHandler"
+    >
+      {{ label }}
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    clickHandler() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
