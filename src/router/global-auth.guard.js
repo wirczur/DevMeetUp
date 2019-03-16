7bf10d181500bcf9292b72101f8951497a7b82e2
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 10:26:49 +0100

    (fix): Poprawa autoryzacji

--- src/router/global-auth.guard.js
+++ src/router/global-auth.guard.js
@@ -3,8 +3,15 @@ import router from '../router';
 router.beforeEach((to, from, next) => {
   const { localStorage } = window;
   const isAdmin = localStorage.getItem('isAdmin');
-  if (isAdmin) {
-    next();
+  console.log(to);
+  if (to.name === 'settings') {
+    if (isAdmin) {
+      next();
+    } else {
+      next({
+        name: 'home',
+      });
+    }
   } else {
     next();
   }
