dec69412cf97f42c86bc639656a206075493d8ba
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 14:19:23 +0100

    [#4.4] Dodanie modułu Setting

---
+++ src/store/modules/settings.module.js
@@ -0,0 +1,22 @@
+
+const initialState = () => ({
+});
+
+const state = initialState;
+
+const getters = {
+};
+
+const mutations = {
+};
+
+const actions = {
+};
+
+export default {
+  namespaced: true,
+  getters,
+  actions,
+  mutations,
+  state,
+};
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 
 import VuexPersistence from 'vuex-persist';
 import user from './modules/user.module';
+import settings from './modules/settings.module';
 
 Vue.use(Vuex);
 
@@ -14,6 +15,7 @@ const vuexLocal = new VuexPersistence({
 export default new Vuex.Store({
   modules: {
     user,
+    settings,
   },
   plugins: [vuexLocal.plugin],
 });
