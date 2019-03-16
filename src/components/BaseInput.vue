4d111a864bb45545e1941e4e9ff4f5f13f1cc618
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 11:45:10 +0100

    [#2.2] Dodanie pola

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="base-input">
+    <el-input
+      v-model="value"
+      type="text"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+  },
+  data() {
+    return {
+      value: null,
+    };
+  },
+  watch: {
+    value(newValue) {
+      this.$emit('change', newValue);
+    },
+  },
+};
+</script>
