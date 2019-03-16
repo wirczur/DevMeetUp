47af09842f62ed9f6f31e74163835f8ec4a471c9
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 11:43:57 +0100

    (none): Dodanie nowego pakietu

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App';
 import router from './router';
 import store from './store';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
