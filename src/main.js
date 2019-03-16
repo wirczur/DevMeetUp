33f96e6247945fd8bb28eca213c9615befd110e4
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 10:55:03 +0100

    (feature): Dodanie nowego pakietu oraz jego konfiguracja

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import App from './App';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
