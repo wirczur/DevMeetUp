952f426159e3377556419811d6072ef6b7e8cfab
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 10:38:41 +0100

    (fix): Przeniesienie guarda na autoryzacje

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,3 +3,22 @@
     <h1>Setting</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Setting',
+
+  beforeRouteEnter(to, from, next) {
+    const { localStorage } = window;
+    const isAdmin = localStorage.getItem('isAdmin') === 'true';
+    if (isAdmin) {
+      next();
+    } else {
+      next({
+        name: 'home',
+      });
+    }
+  },
+};
+
+</script>
