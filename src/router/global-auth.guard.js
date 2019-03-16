d0f64162b68a243e1003a06fe3d137a6f7fd8bb2
Michał Wieczorek <michal.wieczorek@telestrada.pl>
2019-03-16 10:27:57 +0100

    (none): Usunięcie console.loga

--- src/router/global-auth.guard.js
+++ src/router/global-auth.guard.js
@@ -3,7 +3,6 @@ import router from '../router';
 router.beforeEach((to, from, next) => {
   const { localStorage } = window;
   const isAdmin = localStorage.getItem('isAdmin');
-  console.log(to);
   if (to.name === 'settings') {
     if (isAdmin) {
       next();
